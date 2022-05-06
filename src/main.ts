17ea6d1801d8eb7227c40d9ad3170b3a9f435dd3
Bram Adl <thenamesbram@gmail.com>
2022-05-06 22:31:13 +0700

    chore: configure vue slider and floating vue library options

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,24 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 
+import VueSlider from "vue-slider-component";
+import FloatingVue from "floating-vue";
+
 import App from "./App.vue";
 import router from "./router";
+
+import "floating-vue/dist/style.css";
 import "./assets/css/tailwind.css";
+
+FloatingVue.options.distance = 8;
+FloatingVue.options.themes.tooltip.delay.show = 1000;
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(FloatingVue);
+
+app.component("VueSlider", VueSlider);
 
 app.mount("#app");
