90a9595d7597e31ea76d2625febde18ad1315b29
Bram Adl <thenamesbram@gmail.com>
2022-05-06 22:31:29 +0700

    chore: add tailwind/line-clamp plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        base: {
+          gray: "#b3b3b3",
+        },
+      },
+    },
   },
-  plugins: [],
+  // eslint-disable-next-line no-undef
+  plugins: [require("@tailwindcss/line-clamp")],
 };
